7f66d421838f9c0c49df0edf2c27c654d492e5b4
hhd418 <hhd418@gmail.com>
2013-09-24 16:18:05 -0500

    Use foundation for home nav bar. Next step is to figure out how to redirect.

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ ConnectedIn::Application.routes.draw do
   
   root 'students#show_all'
 
-  get "/students" => 'students#show_all'
+  get "/students" => 'students#show_all', as: 'show_student'
 
   get '/students/justin' => 'students#show_justin', as: 'justin_student'
   get '/students/michael' => 'students#show_michael', as: 'michael_student'
   get '/students/yasmin' => 'students#show_yasmin', as: 'yasmin_student'
 
+  # redirect_to 'http://www.rubyonrails.org'
 end
